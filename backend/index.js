4279aef2fa7e33adbebb689f7687a4a7649785d3
Jennycj <ezeobijennifer2@gmail.com>
2022-03-25 16:36:29 +0100

    initialize backend and lightning server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv')
 
+const app = express();
+dotenv.config()
+
+
+// Initialize node & server
+console.log('Initializing Lightning node...');
+initNode().then(() => {
+  console.log('Lightning node initialized!');
+  console.log('Starting server...');
+  app.listen(env.PORT, () => {
+    console.log(`Server started at http://localhost:${process.env.PORT}!`);
+  });
+});
