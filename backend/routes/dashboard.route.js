38e6b0b4aa4e2be517e582c07f2426d82bba26fc
tvpeter <withtvpeter@gmail.com>
2022-03-28 15:03:28 +0100

    refactor dashboard

--- backend/routes/dashboard.route.js
+++ backend/routes/dashboard.route.js
@@ -2,47 +2,46 @@ const express = require("express");
 
 const util = require("../utils/util");
 
-let paidArticleSchema = require("../model/paidArticles.model");
+const paymentSchema = require('../model/payments.model');
+const articleSchema = require('../model/article.model');
 
-const articleRoute = express.Router();
+const dashboardRoute = express.Router();
 
-dashboardRoute.route("/get-paid-articles").get((req, res) => {
+dashboardRoute.route("/").get(async (req, res) => {
+
     const address = req.query.address
-    paidArticleSchema.find({userLnAddress: address},(error, data) => {
-       if(error) {
-           return util.sendError(res, 400, error)
-       } else {
-           return util.sendSuccess(res, 200, data)
-       }
-   }
-   );
-})
 
-dashboardRoute.route("/get-subscribed-users").get((req, res) => {
-    const address = req.query.address
-    paidArticleSchema.find({author: address}, (error, data) => {
-        if(error) {
-            return util.sendError(res, 400, error)
-        } else {
-            let subscriptionCount = data.length+=1
-            return util.sendSuccess(res, 200, subscriptionCount)
-       }
-    })
-});
+    try {
+        const myArticles = await articleSchema.find({author: address});
 
+        let count = 0;
+        let subscribedUsers = 0;
 
+        for (let article of myArticles) {
+            let returnedArticle = await paymentSchema.findById(article.articleId);
 
-dashboardRoute.route("/get-subscription-balance").get(async (req, res) => {
-    const address = req.query.address
-    paidArticleSchema.find({author: address}, (error, data) => {
-        if(error) {
-            return util.sendError(res, 400, error)
-        } else {
-            let subscriptionCount = data.length+=1
-            let subscriptionTotal = subscriptionCount * 10
-            return util.sendSuccess(res, 200, subscriptionTotal);
+            if(returnedArticle){
+                count += 1;
+                subscribedUsers += 1;
+            }
         }
-    })
+
+        const subscriptionAmount = count * 10;
+
+        const result = {
+            subscriptions: count,
+            subscribers: subscribedUsers,
+            subscriptionAmount,
+        }
+        return util.sendSuccess(res, 200, result);
+        
+    } catch (error) {
+        return util.sendError(res, 400, error)
+        
+    }
+
+    
+    
 })
 
 module.exports = dashboardRoute;
